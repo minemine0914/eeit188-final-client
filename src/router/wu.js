a671195207f602b4365d6cb24b91c57a938c2e66
蔣孟華 (Leon Chiang) <50078157+CHIANGMENGHUA@users.noreply.github.com>
2024-09-11 17:29:41 +0800

    component: () => import("@/components/wu/Test.vue")

--- src/router/wu.js
+++ src/router/wu.js
@@ -1,15 +1,15 @@
 const router = {
-    path: "/wu",
-    name: "Wu",
-    component: () => import("@/components/wu/test.vue"),
-    children: [
-        {
-            path: "/aaa", // Default
-            name: "WuHome",
-            component: () => import("@/components/wu/components/EmptyTemplate.vue"),
-            meta: { title: "Wu Testing", requiresAuth: false },
-        },
-    ],
-}
+  path: "/wu",
+  name: "Wu",
+  component: () => import("@/components/wu/Test.vue"),
+  children: [
+    {
+      path: "/aaa", // Default
+      name: "WuHome",
+      component: () => import("@/components/wu/components/EmptyTemplate.vue"),
+      meta: { title: "Wu Testing", requiresAuth: false },
+    },
+  ],
+};
 
 export default router;
