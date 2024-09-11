78250b441faa7ed284ccce581e69a18ab3523ed0
strawberry1515 <straw15berry@gmail.com>
2024-09-11 15:24:38 +0800

    FrameDemo大架構

---
+++ src/components/wu/frame/FrameDemo.vue
@@ -0,0 +1,31 @@
+<template>
+    <div>
+        <h2>房東報表首頁</h2>
+    </div>
+
+    <div>
+        <p>選擇房源</p>
+        <p>全部/ooo/xxx/...</p>
+        <select name="" id="">
+            <option value=""></option>
+        </select>
+    </div>
+
+    <p>顯示模式：文字/圖形</p>
+    <select name="" id=""></select>
+
+    <p>時間區段</p>
+    <p>年度：</p>
+    <p>季度：</p>
+    <p>月份：</p>
+
+    <select name="" id=""></select>
+
+    客戶分析 pie
+    房源收入 v-sparkline
+    入住率
+</template>
+
+<script setup></script>
+
+<style scoped></style>
