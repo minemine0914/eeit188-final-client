67fdeb671b45f27c6e78ddfbc4066642116cd86a
MineMine <59837822+minemine0914@users.noreply.github.com>
2024-09-12 18:28:39 +0800

    Update HouseDetail.vue

--- src/components/user/HouseDetail.vue
+++ src/components/user/HouseDetail.vue
@@ -1,10 +1,10 @@
 <template>
     <v-container v-if="isErrorGetHouseInfo">
         House not found
+        <v-btn to="/house/qwe">TEST1</v-btn>
+        <v-btn to="/house/asd">TEST1</v-btn>
+        <v-btn to="/house/f4d671db-d560-46b6-8606-276df8f8cadc">TEST1</v-btn>
     </v-container>
-    <v-btn to="/house/qwe">TEST1</v-btn>
-    <v-btn to="/house/asd">TEST1</v-btn>
-    <v-btn to="/house/f4d671db-d560-46b6-8606-276df8f8cadc">TEST1</v-btn>
     <v-container v-if="!isErrorGetHouseInfo">
         <v-row class="pa-1">
             <v-col cols="12">
@@ -15,9 +15,14 @@
         </v-row>
         <v-row no-gutters>
             <v-col cols="6">
-                <v-responsive :aspect-ratio="5 / 4">
-                    <v-sheet class="h-100 w-100 pa-1" >
-                        <v-img :src="testImg" position="center" class="h-100" cover rounded></v-img>
+                <v-responsive :aspect-ratio="10 / 9">
+                    <v-sheet class="h-100 w-100 pa-1">
+                        <v-img
+                            :src="testImg"
+                            position="center"
+                            class="h-100 rounded-s-xl"
+                            cover
+                        ></v-img>
                     </v-sheet>
                 </v-responsive>
             </v-col>
@@ -25,24 +30,34 @@
                 <v-row no-gutters class="h-50">
                     <v-col cols="6">
                         <v-sheet color=" pa-1 h-100 w-100">
-                            <v-img :src="testImg" position="center" class="h-100" cover rounded></v-img>
+                            <v-img :src="testImg" position="center" class="h-100" cover></v-img>
                         </v-sheet>
                     </v-col>
                     <v-col cols="6">
                         <v-sheet color=" pa-1 h-100 w-100">
-                            <v-img :src="testImg" position="center" class="h-100" cover rounded></v-img>
+                            <v-img
+                                :src="testImg"
+                                position="center"
+                                class="h-100 rounded-te-xl"
+                                cover
+                            ></v-img>
                         </v-sheet>
                     </v-col>
                 </v-row>
                 <v-row no-gutters class="h-50">
                     <v-col cols="6">
                         <v-sheet color=" pa-1 h-100 w-100">
-                            <v-img :src="testImg" position="center" class="h-100" cover rounded></v-img>
+                            <v-img :src="testImg" position="center" class="h-100" cover></v-img>
                         </v-sheet>
                     </v-col>
                     <v-col cols="6">
                         <v-sheet color=" pa-1 h-100 w-100">
-                            <v-img :src="testImg" position="center" class="h-100" cover rounded></v-img>
+                            <v-img
+                                :src="testImg"
+                                position="center"
+                                class="h-100 rounded-be-xl"
+                                cover
+                            ></v-img>
                         </v-sheet>
                     </v-col>
                 </v-row>
@@ -50,7 +65,7 @@
         </v-row>
         <v-row class="pa-1">
             <v-col cols="12" md="8">
-                <v-sheet border>TEST</v-sheet>
+                <v-sheet border>TEST </v-sheet>
             </v-col>
             <v-col cols="12" md="4">
                 <v-sheet border>TEST</v-sheet>
