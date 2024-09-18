a640c0248f017935292861d22f1a7634333e59ad
MineMine <59837822+minemine0914@users.noreply.github.com>
2024-09-18 15:51:59 +0800

    加入其他的分享來源

--- src/pages/home/Search.vue
+++ src/pages/home/Search.vue
@@ -117,12 +117,12 @@
                                             </div>
                                             <div class="text-grey-darken-1">
                                                 <span class="mr-2" v-if="!item.pet">
-                                                    <span class="mdi mdi-paw-off mr-1"></span>
+                                                    <span class="mdi mdi-paw-off mr-2"></span>
                                                     <span>禁止寵物</span>
                                                 </span>
 
                                                 <span class="mr-2" v-if="!item.smoke">
-                                                    <span class="mdi mdi-smoking-off mr-1"></span>
+                                                    <span class="mdi mdi-smoking-off mr-2"></span>
                                                     <span>禁止吸菸</span>
                                                 </span>
                                             </div>
