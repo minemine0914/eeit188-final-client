96ce810ffd3934ba90e2545473a519664e15d59d
MineMine <59837822+minemine0914@users.noreply.github.com>
2024-09-23 11:20:46 +0800

    更新 路由守衛 修正一些東西

---
+++ src/components/home/DiscussInfinityScroll.vue
@@ -0,0 +1,19 @@
+<template>
+    <v-infinite-scroll :height="300" :items="items" :onLoad="load">
+        <template v-for="(item, index) in items" :key="item">
+            <div :class="['pa-2', index % 2 === 0 ? 'bg-grey-lighten-2' : '']">
+                Item number #{{ item }}
+            </div>
+        </template>
+    </v-infinite-scroll>
+</template>
+<script setup>
+import { storeToRefs } from 'pinia';
+import { useHouseDetailStore } from '../../stores/houseDetailStore';
+
+
+const houseDetailStore = useHouseDetailStore();
+const  { discussList } = storeToRefs(houseDetailStore);
+
+</script>
+<style scoped></style>
