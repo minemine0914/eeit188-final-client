b4fc6b4164f5cc83d7fb2f1c66fdf7c70825d05a
VJKenjo <mit123000222@gmail.com>
2024-09-23 18:54:06 +0800

    路徑

--- src/components/kenjo/Home.vue
+++ src/components/kenjo/Home.vue
@@ -2,13 +2,13 @@
     <div>
       <h1>Home</h1>
       <nav>
-        <router-link to="/System/BookingManagement"><v-btn>訂單</v-btn></router-link>
-        <router-link to="/System/UserManagement"><v-btn>使用者</v-btn></router-link>
-        <router-link to="/System/FinancialManagement"><v-btn>當日金流</v-btn></router-link>
-        <router-link to="/System/PromotionsandDiscountsManagement"><v-btn>優惠券</v-btn></router-link>
-        <router-link to="/System/PropertyApproval"> <v-btn>審核</v-btn></router-link>
-        <router-link to="/System/RoomManagement"> <v-btn>房屋</v-btn></router-link>
-        <!-- <router-link to="/System/Test"><v-btn>測試</v-btn></router-link> -->
+        <router-link to="/system/BookingManagement"><v-btn>訂單</v-btn></router-link>
+        <router-link to="/system/UserManagement"><v-btn>使用者</v-btn></router-link>
+        <router-link to="/system/FinancialManagement"><v-btn>當日金流</v-btn></router-link>
+        <router-link to="/system/PromotionsandDiscountsManagement"><v-btn>優惠券</v-btn></router-link>
+        <router-link to="/system/PropertyApproval"> <v-btn>審核</v-btn></router-link>
+        <router-link to="/system/RoomManagement"> <v-btn>房屋</v-btn></router-link>
+        <!-- <router-link to="/system/Test"><v-btn>測試</v-btn></router-link> -->
         
         
       </nav>
