d7235bede866deb1fdce039362adbad7ed5184bb
蔣孟華 (Leon Chiang) <50078157+CHIANGMENGHUA@users.noreply.github.com>
2024-10-01 10:05:49 +0800

    添加 sweetAlert2

--- src/views/HostView.vue
+++ src/views/HostView.vue
@@ -124,6 +124,7 @@ import { useRouter } from "vue-router";
 import { useUserViewStore } from "../stores/userViewStore";
 import { useUserStore } from "../stores/userStore";
 import { storeToRefs } from "pinia";
+import Swal from "sweetalert2";
 
 const menuItems = ref([
   { title: "管理房源", path: "/lo/property-management" },
@@ -166,11 +167,19 @@ function onResize() {
 
 // 登出
 function handleLogout() {
-  const confirmLogout = window.confirm("請確認是否要登出");
-
-  if (confirmLogout) {
-    logout();
-  }
+  Swal.fire({
+    title: "請確認是否要登出?",
+    icon: "question",
+    showCancelButton: true,
+    confirmButtonColor: "#3085d6",
+    cancelButtonColor: "#d33",
+    confirmButtonText: "確定",
+    cancelButtonText: "取消",
+  }).then((result) => {
+    if (result.isConfirmed) {
+      userStore.logout();
+    }
+  });
 }
 </script>
 <style scoped></style>
