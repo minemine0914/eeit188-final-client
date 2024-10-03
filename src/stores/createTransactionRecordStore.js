527b5bf7fdc828072fe80e02777d732747c4ffad
strawberry1515 <straw15berry@gmail.com>
2024-10-03 13:01:21 +0800

    建立假交易 /wu/create 頁面
    照順序1.抓user 2.抓house 3.建立交易
    等textbox出現ID字串才代表抓完資料，才可以點下一個
    
    **如果需要隨機產生交易時間，後端有更新，要pull後端

--- src/stores/createTransactionRecordStore.js
+++ src/stores/createTransactionRecordStore.js
@@ -7,7 +7,8 @@ export const useCreateTransactionRecordStore = defineStore('createTransactionRec
     state: () => ({
         usersResult: '',
         housesResult: '',
-        dataAmount: 10,
+        dataAmount: 100,
+        progress: '',
     }),
     actions: {
         async findAllUserString() {
@@ -15,6 +16,7 @@ export const useCreateTransactionRecordStore = defineStore('createTransactionRec
             try {
                 const response = await api.get(`/user/find-users?pageSize=1000`);
                 this.usersResult = response.data.users.map(user => user.id || 0);
+                console.log('users', this.usersResult)
             } catch (error) {
                 console.error('Error fetching users:', error);
             }
@@ -24,6 +26,8 @@ export const useCreateTransactionRecordStore = defineStore('createTransactionRec
             try {
                 const response = await api.get(`/house/all?limit=1000`);
                 this.housesResult = response.data.content.map(house => house.id || 0);
+                console.log('houses', this.housesResult)
+
             } catch (error) {
                 console.error('Error fetching houses:', error);
             }
@@ -32,9 +36,12 @@ export const useCreateTransactionRecordStore = defineStore('createTransactionRec
         async crossInsertTransactionRecord() {
             for (let i = 0; i < this.dataAmount; i++) {
                 try {
+                    this.progress = `開始建立交易(${i + 1}/${this.dataAmount})`
+                    console.log(`開始建立交易(${i + 1}/${this.dataAmount})`)
+
                     const houseId = this.housesResult[Math.floor(Math.random() * this.housesResult.length)];
                     const userId = this.usersResult[Math.floor(Math.random() * this.usersResult.length)];
-                    let cashflow = Math.random() * 10000
+                    // let cashflow = Math.random() * 10000
                     // console.log(new Date(Date.now() - Math.random() * 86400 * 1000 * 365 * 10))
                     let date = new Date(Date.now() - Math.random() * 86400 * 1000 * 365 * 10)
                     const pad = (num) => String(num).padStart(2, '0');
@@ -48,40 +55,27 @@ export const useCreateTransactionRecordStore = defineStore('createTransactionRec
                     const milliseconds = String(date.getUTCMilliseconds()).padStart(3, '0');
                     // 2024-09-24 17:54:07.2490000
                     date = `${year}-${month}-${day}T${hours}:${minutes}:${seconds}.${milliseconds}+00:00`;
-                    console.log(date)
+                    // console.log(date)
 
-                    let longTime = Date.now() - Math.random() * 86400 * 1000 * 365
+                    let longTime = Date.now() - Math.random() * 86400 * 1000 * 365 * 3 // 訂房起始日設為3年內開始
                     let startDate = new Date(longTime)
-                    let endDate = new Date(Math.min(longTime + Math.random() * 86400 * 1000 * 365, startDate))
-
+                    let endDate = new Date(Math.min(longTime + Math.random() * 86400 * 1000 * 5, startDate)) // 住1~5天
 
                     const response = await api.post("/payment/booking-house", {
                         houseId: houseId,
                         userId: userId,
                         // couponId: couponId ? couponId : null,
                         dateRange: [startDate, endDate],
+                        createdAt: startDate,
                     });
-                    return response.data;
+                    console.log('建立成功：', response.data)
                 } catch (error) {
-                    // console.error("Error creating order:", error);
-                    throw error;
+                    console.error("Error creating order:", error);
+                    // throw error;
                 }
 
-
-
-                //     await api.post(`/transcation_record/`, {
-                //         houseId,
-                //         userId,
-                //         cashFlow: cashflow,
-                //         deal: "確認付款中",
-                //         platformIncome: cashflow * 0.05,
-                //         "createdAt": date,
-                //     });
-                //     console.log("created", houseId, userId)
-                // } catch (error) {
-                //     console.error('Error inserting transaction record:', error);
-                // }
             }
+            this.progress = `已建立${this.dataAmount}筆資料`
         },
     }
 });
